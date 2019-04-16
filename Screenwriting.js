4238108663364231ce911054648a382a1abe627f
Austin M Graff <ag73776@srcs.loc>
2019-04-16 14:22:47 -0700

    Added some attributes

--- Screenwriting.js
+++ Screenwriting.js
@@ -1,15 +1,26 @@
 class Screenplay {
-
+    constructor(){
+        this.document= new Document();
+    }
 }
 
 class Document {
-
+    constructor(){
+        this.text = new Text();
+    }
 }
 
 class Text {
-
+    constructor(){
+        this.font = 1;
+        this.spacing = 1;
+        this.written = " ";
+        this.keycommands = new Key
+    }
 }
 
 class Key {
+    constructor(){
 
+    }
 }
