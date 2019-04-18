9225baa53b5724bc1fb907b2bb93b963f784e762
Austin M Graff <ag73776@srcs.loc>
2019-04-18 13:50:37 -0700

    Added the function changeFont()

--- Screenwriting.js
+++ Screenwriting.js
@@ -18,6 +18,21 @@ class Text {
         this.keycommands = new Key
 
     }
+ document.addEventListener("keypress", changeFont);
+
+function changeFont(){
+    if (keycommands.ctrlKey){
+      if(event.key = "b" || event.key = "B"){
+          this.font = 2;
+      }
+      if(event.key = "i" || event.key = "I"){
+          this.font = 3;
+      }
+      if(event.key = "j" || event.key = "J"){
+          this.font = 4;
+      }
+    }
+}
 }
 
 class Key {
