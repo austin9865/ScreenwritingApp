bf2bc84bc92dbf5883a357eeb085dcf869dd0b64
Jaime R Yah-Perez <jy73802@srcs.loc>
2019-04-22 14:40:17 -0700

    made changes to the events in shortcut function

--- Screenwriting.js
+++ Screenwriting.js
@@ -16,32 +16,17 @@ class Text {
     constructor(){
         this.written = " ";
         this.keyCommands = new Command;
-        this.keyInput = new Key;
-
+        this.keyInput = new Key();
     }
-
-document.addEventListener("keypress", changeFont);
- document.addEventListener("keypress",  changeSpacing);
-
-
-}
-
-
-class Key {
-    constructor(){
-    this.letters = "";
-    }
-
-document.addEventListener("keypress", inputKey);
-
-    function inputKey(){
-        this.letters += event.key;
+    fuction inputKey(){
+        this.letters += Event.key;
         console.log(this.letters);
     }
+
+document.addEventListener("keypress", inputKey())
+document.addEventListener("keydown", inputkey())
 }
 
-document.addEventListener("keypress", changeFont);
-document.addEventListener("keypress",  changeSpacing);
 
 class Command {
     constructor(){
@@ -51,39 +36,40 @@ class Command {
 
     function shortcuts(){
     if (KeyboardEvent.ctrlKey){
-      if(event.key = "q" || event,key = "Q"){
+      if(Event.key = "q" || Event,key = "Q"){
           this.font = 1;
       }
-      if(event.key = "w" || event.key = "W"){
+      if(Event.key = "w" || Event.key = "W"){
           this.font = 2;
       }
-      if(event.key = "e" || event.key = "E"){
+      if(Event.key = "e" || Event.key = "E"){
           this.font = 3;
       }
-      if(event.key = "r" || event.key = "R"){
+      if(Event.key = "r" || Event.key = "R"){
           this.font = 4;
       }
         if(Event.key = "t" || Event.key = "T"){
             this.spacing = 1;
         }
-        if(event.key = "y" || event.key = "Y"){
+        if(Event.key = "y" || Event.key = "Y"){
             this.spacing = 2;
         }
-        if(event.key = "u" || event.key = "U"){
+        if(Event.key = "u" || Event.key = "U"){
             this.spacing = 3;
         }
-        if(event.key = "i" || event.key = "I"){
+        if(Event.key = "i" || Event.key = "I"){
             this.spacing = 4;
         }
-        if(event.key = "o" || event.key = "O"){
+        if(Event.key = "o" || Event.key = "O"){
             this.spacing = 5;
         }
-        if(event.key = "p" || event.key = "P"){
+        if(Event.key = "p" || Event.key = "P"){
             this.spacing = 6;
         }
-    } else if(Event.keyCode = 13){
+    } if(Event.keyCode = 13){
 
-    }
+    } if()
 }
 }
-
+ document.addEventListener("keypress", changeFont);
+ document.addEventListener("keypress",  changeSpacing);
