73771d54ff8434672dec055ae9f55f7fccb3799b
Jaime R Yah-Perez <jy73802@srcs.loc>
2019-04-26 13:22:01 -0700

    changed command class and added new commands

--- Screenwriting.js
+++ Screenwriting.js
@@ -8,20 +8,23 @@ class Screenplay {
 
 class Document {
     constructor(){
-        this.text = new Line();
+        this.act = new Act();
     }
 }
 class Act{
-
+    constructor(){
+    this.scene = new Scene();
+}
 }
 class Scene{
-
+    constructor(){
+        this.text = new Line();
+    }
 }
 class Line {
     constructor(){
         this.written = " ";
-        //this.keyCommands = new Command;
-        //this.keyInput = new Key;
+        //this.keyCommands = new Command();
         window.addEventListener("keypress", (event) => {
             this.inputKey(event);
         });
@@ -50,34 +53,33 @@ class Line {
 
 
 }
-//}
-//window.addEventListener("keydown", shortcuts);
-//
 //class Command {
 //    constructor(){
 //        this.font = 1;
 //        this.spacing = 1;
+//  window.addEventListener("keydown", shortcuts);
 //    }
 //
 //    function shortcuts(){
 //    if (KeyboardEvent.ctrlKey){
 //      if(Event.key = "q" || Event,key = "Q"){
-//          this.font = 1;
+//          this.font = 1 + 1;
 //      }
-//      if(Event.key = "w" || Event.key = "W"){
-//          this.font = 2;
+//      if(Event.key = "b" || Event.key = "B"){
+//         this.doc.style.fontWeight = "bold";
 //      }
-//      if(Event.key = "e" || Event.key = "E"){
-//          this.font = 3;
+//      if(Event.key = "i" || Event.key = "I"){
+//          this.doc.style.fontStyle = "italic";
 //      }
 //      if(Event.key = "r" || Event.key = "R"){
-//          this.font = 4;
+//          this.doc.style.fonstyle = "";
+//          this.doc.style.fontWeight = "";
 //      }
 //        if(Event.key = "t" || Event.key = "T"){
-//            this.spacing = 1;
+//            this.spacing = 1 + 1;
 //        }
 //        if(Event.key = "y" || Event.key = "Y"){
-//            this.spacing = 2;
+//            this.doc.style.textTransform = "capitalize";
 //        }
 //        if(Event.key = "u" || Event.key = "U"){
 //            this.spacing = 3;
@@ -94,6 +96,7 @@ class Line {
 //    } else if(Event.keyCode = 13){
 //        this.lines = new Line();
 //    }
+//      if(){}
 //}
 //}
 
