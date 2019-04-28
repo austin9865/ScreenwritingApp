822c98a55ca9f10edaf03790ded4c8e13afbd78d
JaimeYahPerez <dogemlg42@gmail.com>
2019-04-27 18:37:57 -0700

    fixed the command class and commented what is still causing issues with writing

--- Screenwriting.js
+++ Screenwriting.js
@@ -24,7 +24,7 @@ class Scene{
 class Line {
     constructor(){
         this.written = " ";
-        //this.keyCommands = new Command();
+        this.keyCommands = new Command();
         window.addEventListener("keypress", (event) => {
             this.inputKey(event);
         });
@@ -50,22 +50,21 @@ class Line {
         }
         doc.textContent = this.written;
     }
-
-
 }
-//class Command {
-//    constructor(){
-//        this.font = 1;
-//        this.spacing = 1;
-//  window.addEventListener("keydown", shortcuts);
-//    }
-//
-//    function shortcuts(){
-//    if (KeyboardEvent.ctrlKey){
-//      if(Event.key = "q" || Event,key = "Q"){
-//          this.doc.style.fontSize = 1 + 1 + "px";
-//          this.font = 1 + 1;
-//      }
+class Command {
+    constructor(){
+        this.font = 1;
+        this.spacing = 1;
+  window.addEventListener("keydown", (event) => {
+      this.shortcuts(event)});
+    }
+
+  shortcuts(KeyboardEvent){
+      if (KeyboardEvent.shiftKey){
+      if(Event.key = "q" || Event,key = "Q"){
+          this.doc.style.fontSize = 1 + 1 + "px";
+          this.font = 1 + 1;
+      }
 //      if(Event.key = "b" || Event.key = "B"){
 //         this.doc.style.fontWeight = "bold";
 //      }
@@ -79,29 +78,19 @@ class Line {
 //        if(Event.key = "t" || Event.key = "T"){
 //            this.spacing = 1 + 1;
 //        }
-//        if(Event.key = "y" || Event.key = "Y"){
+//        if(Event.key = "a" || Event.key = "A"){
 //            this.doc.style.textTransform = "capitalize";
 //        }
-//        if(Event.key = "u" || Event.key = "U"){
-//            this.spacing = 3;
-//        }
-//        if(Event.key = "i" || Event.key = "I"){
-//            this.spacing = 4;
-//        }
-//        if(Event.key = "o" || Event.key = "O"){
-//            this.spacing = 5;
-//        }
-//        if(Event.key = "p" || Event.key = "P"){
-//            this.spacing = 6;
-//        }
-//    } else if(Event.keyCode = 13){
-//        this.lines = new Line();
-//    }
-//      if(this.font >= 5){
-//      this.doc.style.fontSize = 1 + "px";
-//      this.font = 1;
-//}
-//}
+          if(Event.keyCode = 13){
+              this.lines = new Line();
+          }
+      if(this.font >= 5){
+      this.doc.style.fontSize = 1 + "px";
+      this.font = 1;
+}
+}
+}
+}
 
 
 let screenplay = new Screenplay();
